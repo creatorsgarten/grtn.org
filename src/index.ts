1656d7f3ec50012806df689db959aef972d55d84
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-07 23:28:03 +0700

    ‌

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ export interface Env {
   //
   // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
   // MY_SERVICE: Fetcher;
+
+  AMPLITUDE_API_KEY: string;
+}
+
+interface RedirectRoute {
+  from: string;
+  to: string;
+  definition: string;
 }
 
 export default {
@@ -18,37 +26,10 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const [shortcuts, redirects] = await Promise.all([
-      searchWiki({ grtn: true }),
-      searchWiki({ grtnRedirects: true }),
-    ]);
-    const pages = [
-      ...shortcuts.result.data.results,
-      ...redirects.result.data.results,
-    ];
-
-    const routes: { from: string; to: string }[] = [];
-    for (const page of pages) {
-      const pageRef = page.pageRef;
-
-      const shortcut = page.frontMatter?.grtn;
-      if (typeof shortcut === "string") {
-        routes.push({
-          from: shortcut,
-          to: wikiUrl(pageRef),
-        });
-      }
-
-      const redirects = page.frontMatter?.grtnRedirects;
-      if (redirects && typeof redirects === "object") {
-        for (const [from, to] of Object.entries(redirects)) {
-          if (typeof from === "string" && typeof to === "string") {
-            routes.push({ from, to });
-          }
-        }
-      }
-    }
-
+    const pathname = new URL(request.url).pathname;
+    const amplitudePromise = trackVisit(request, env, pathname);
+    const routes = await getRoutes();
+    await amplitudePromise;
     return new Response(JSON.stringify(routes, null, 2), {
       headers: {
         "content-type": "application/json;charset=UTF-8",
@@ -56,6 +37,41 @@ export default {
     });
   },
 };
+
+async function getRoutes() {
+  const [shortcuts, redirects] = await Promise.all([
+    searchWiki({ grtn: true }),
+    searchWiki({ grtnRedirects: true }),
+  ]);
+  const pages = [
+    ...shortcuts.result.data.results,
+    ...redirects.result.data.results,
+  ];
+
+  const routes: RedirectRoute[] = [];
+  for (const page of pages) {
+    const pageRef = page.pageRef;
+
+    const shortcut = page.frontMatter?.grtn;
+    if (typeof shortcut === "string") {
+      routes.push({
+        from: shortcut,
+        to: wikiUrl(pageRef),
+        definition: wikiUrl(pageRef),
+      });
+    }
+
+    const redirects = page.frontMatter?.grtnRedirects;
+    if (redirects && typeof redirects === "object") {
+      for (const [from, to] of Object.entries(redirects)) {
+        if (typeof from === "string" && typeof to === "string") {
+          routes.push({ from, to, definition: wikiUrl(pageRef) });
+        }
+      }
+    }
+  }
+  return routes;
+}
 
 async function searchWiki(query: any) {
   const response = await fetch(
@@ -78,4 +94,30 @@ function wikiUrl(pageRef: string) {
     return `https://creatorsgarten.org/event/${pageRef.slice(7)}`;
   }
   return `https://creatorsgarten.org/wiki/${pageRef}`;
+}
+
+async function trackVisit(request: Request, env: Env, pathname: string) {
+  const amplitudeApiKey = env.AMPLITUDE_API_KEY;
+  if (!amplitudeApiKey) {
+    return;
+  }
+  const response = await fetch("https://api2.amplitude.com/2/httpapi", {
+    method: "POST",
+    body: JSON.stringify({
+      api_key: amplitudeApiKey,
+      events: [
+        {
+          user_id: "anonymous_user",
+          event_type: "visit",
+          event_properties: {
+            pathname,
+          },
+          ip: request.headers.get("cf-connecting-ip"),
+        },
+      ],
+    }),
+  });
+  if (!response.ok) {
+    console.warn("Failed to track visit", response.status);
+  }
 }
